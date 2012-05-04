e1871ad9bb9d64f8e9fef4344ee3593c04ca67f6
Eric Cranston <cracell@gmail.com>
2012-05-03 19:54:15 -0700

    More cleanup to the JavaScript. No longer leaking variables out

--- javascripts/reddit_leanback.js
+++ javascripts/reddit_leanback.js
@@ -1,14 +1,17 @@
 RedditLeanback = {};
 
 RedditLeanback.load = function() {
+  $('.ajax-loader').show();
   var baseURL,
-      urlPath = RedditLeanback.pathFromURL();
+      urlPath = RedditLeanback.pathFromURL(),
+      subreddit = document.getElementById('subreddit').value,
+      limit = document.getElementById('limit').value;
   if (urlPath.length > 3) {
      baseURL = $.urlParam('url');
    } else if (subreddit != '') {
-     baseURL = 'r/' + subreddit + '.json?limit=' + limit + '&jsonp=loadImages';
+     baseURL = 'r/' + subreddit + '.json?limit=' + limit + '&jsonp=RedditLeanback.processJSON';
    } else {
-     baseURL = '.json?limit=' + limit + '&jsonp=loadImages'
+     baseURL = '.json?limit=' + limit + '&jsonp=RedditLeanback.processJSON'
    }
    var url = decodeURIComponent(baseURL);
    RedditLeanback.requestJSON(url);  
@@ -23,7 +26,7 @@ RedditLeanback.requestJSON = function(url){
 }
 
 RedditLeanback.refresh = function(){
-  document.getElementById('result').innerHtml = '';
+  $('#images').html('');
   RedditLeanback.load();
 }
 
@@ -36,91 +39,60 @@ RedditLeanback.pathFromURL = function(){
   }
 }
 
-
-var subreddit = '', limit = 30, hideNSFW = "true";
-  $(function() {
-    $("a[href='options']").click(function(e) {
-      e.preventDefault();
-      $('#options').toggle();
-    });
-    
-    
-    $('#options-form').submit(function(e) {
-      e.preventDefault();
-      subreddit = $('#subreddit').val();
-      if (parseInt($('#limit').val()) > 0) {
-        limit = $('#limit').val();
-      }
-      RedditLeanback.load();
-    });
-    
-    $('#limit-form').submit(function(e) {
-      e.preventDefault();
-      subreddit = $('#subreddit').val();
-      if (parseInt($('#limit').val()) > 0) {
-        limit = $('#limit').val();
-      }
-      
-      RedditLeanback.load();
-    });
-    
-    $('#nsfw').change(function(){
-      hideNSFW = $('#nsfw').val();
-      RedditLeanback.load();
-    });
-        
-    $('.showNSFW').live('click', function(e) {
-      e.preventDefault();
-      $(this).next().show();
-      $(this).hide();
-    });
-    
-    RedditLeanback.load();
-       
-       $('.more').click(function(e){
-         e.preventDefault();
-         $('.ajax-loader').show();
-         if (base_url.match(/\?/)) {
-           after_op = '&'
-         } else {
-           after_op = '?'
-         }
-         url = base_url + after_op +'after=' + result.after;
-        getImages(url);
-       });
-  });
-  
-
-  
-
-  
-  // Used to check for Preview
-
-  
-function loadImages(res) {
-  result = res.data;
+RedditLeanback.processJSON = function(json){
+  var links = json.data.children;
   $('.ajax-loader').hide();
-  $.each(res.data.children, function(index, value) { 
-    if (value.data.url.match(/imgur/)) {
-          var output = '';
-          output += '<div class="image"><h1>' + value.data.title + '</h1>';
-          var original_url = value.data.url;
-          var split_url = original_url.split('/');
-          var url = '';
-          if (split_url.pop().match(/\./)) {
-           url = original_url;
-          } else {
-            url = original_url + '.jpg';
-          }
-          if (hideNSFW === 'true' && value.data.over_18 === true) {
-            output += '<a href="#" class="showNSFW">Show NSFW Image</a>';
-            var hiddenStyle = 'display:none;';
-          } else {
-            var hiddenStyle = '';
-          }
-       output += '<a href='+ url + ' target="_window" style="' + hiddenStyle + '"><img src="' + url + '" /></a>';
-        output += '<p>source: <a target="_window" href="' + original_url + '">' + original_url + '</a> - <a target="_window" href="http://www.reddit.com' + value.data.permalink + '">Comment at Reddit</a></p></div>';
-      $('#result').append(output);
+  for(var i = 0; i < links.length; i++) {
+    // If the url has imgur in it, try to add the image  
+    if (links[i].data.url.match(/imgur/)) {
+      RedditLeanback.appendImgurImage(links[i].data);
     }
-  });
+  }
 }
+
+RedditLeanback.appendImgurImage = function(image) {
+  var output = '',
+      hideNSFW = document.getElementById('hideNSFW').value,
+      url = '';
+  
+  var original_url = image.url;
+  var split_url = original_url.split('/');
+  
+  output += '<div class="image"><h1>' + image.title + '</h1>';
+  
+  if (split_url.pop().match(/\./)) {
+    url = original_url;
+  } else {
+    url = original_url + '.jpg';
+  }
+  if (hideNSFW === 'true' && image.over_18 === true) {
+    output += '<a href="#" class="showNSFW">Show NSFW Image</a>';
+    var hiddenStyle = 'display:none;';
+  } else {
+    var hiddenStyle = '';
+  }
+  
+  output += '<a href='+ url + ' target="_window" style="' + hiddenStyle + '"><img src="' + url + '" /></a>';
+  output += '<p>source: <a target="_window" href="' + original_url + '">' + original_url + '</a> - <a target="_window" href="http://www.reddit.com' + image.permalink + '">Comment at Reddit</a></p></div>';
+  $('#images').append(output);
+}
+
+$(function() {
+  $('.showNSFW').live('click', function(e) {
+    e.preventDefault();
+    $(this).next().show();
+    $(this).hide();
+  });
+  
+  $("a[href='options']").click(function(e) {
+    e.preventDefault();
+    $('#options').toggle();
+  });
+  
+  $('#refresh').click(function(e) {
+    e.preventDefault();
+    RedditLeanback.refresh();
+  })
+  
+  RedditLeanback.load();
+});
